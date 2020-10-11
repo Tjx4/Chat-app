980eec639fd36e2d4502d98cc0a7171a097faf2b
Tshepo <rocboyt@gmail.com>
2020-10-11 21:29:08 +0200

    Added tableView

--- Chat App/features/friends/FriendsViewController.swift
+++ Chat App/features/friends/FriendsViewController.swift
@@ -1,29 +1,35 @@
 import UIKit
 
-class FriendsTableViewController : UITableViewController {
+class FriendsViewController : UIViewController {
     
-    @IBOutlet var friendsTableView: UITableView!
+    @IBOutlet weak var myFriendsTableView: UITableView!
     
     let names = [
         "Tshepo",
-        "Nomvula"
+        "Nomvula",
+        "Masingita"
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        friendsTableView.delegate = self
-        friendsTableView.dataSource = self
+        myFriendsTableView.delegate = self as! UITableViewDelegate
+        myFriendsTableView.dataSource = self as! UITableViewDataSource
     }
     
 }
 
-extension ViewController: UITableViewDelegate {
+extension FriendsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("indexPath: \(indexPath)")
     }
 }
 
-extension ViewController: UITableViewDataSource {
+extension FriendsViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return names.count
     }
@@ -31,10 +37,8 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = "Hello Friend"
-        //names[indexPath.row]
+        cell.textLabel?.text = names[indexPath.row]
         return cell
     }
-    
     
 }
